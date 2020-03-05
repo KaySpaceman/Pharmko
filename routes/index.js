868331d30398b3b9216994786a31a955fb5e959d
Kristers Lielups <kristers.lielups@gmail.com>
2020-03-05 09:21:59 +0200

    Port current fiels to NodeJS app

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,23 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/aktuali', function(req, res, next) {
+  res.render('aktuali', { title: 'Aktuali' });
+});
+router.get('/davanas', function(req, res, next) {
+  res.render('davanas', { title: 'Davanas' });
+});
+router.get('/kontakti', function(req, res, next) {
+  res.render('kontakti', { title: 'Kontakti' });
+});
+router.get('/loterija', function(req, res, next) {
+  res.render('loterija', { title: 'Loterija' });
+});
+router.get('/parmums', function(req, res, next) {
+  res.render('parmums', { title: 'Par Mums' });
+});
+router.get('/preces', function(req, res, next) {
+  res.render('preces', { title: 'Preces' });
+});
 
 module.exports = router;
