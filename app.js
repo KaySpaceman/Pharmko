97d9c4842acb96225cda132fe94c0c0de0e0a49c
Kristers Lielups <kristers.lielups@gmail.com>
2020-05-25 17:57:45 +0300

    Get products from Database

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+const mongoose = require('mongoose');
+const hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
+
+
+mongoose.connect('mongodb://localhost/Pharmko');
+hbs.registerPartials(`${__dirname}/views/partials`);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +34,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
